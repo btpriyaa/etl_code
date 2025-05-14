d642b04f5cd0d35fd551862f3d887fffef3ac605
priyaa_codez <btpriyaa@gmail.com>
2025-05-14 20:06:11 +0800

    updated config

--- config/config.py
+++ config/config.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel, FilePath
 from pathlib import Path
+
 import yaml
+from pydantic import BaseModel, FilePath
+
 
 class LoggingConfig(BaseModel):
-    level: str # e.g., INFO, DEBUG
-    file: Path # Path to log file
+    level: str  # e.g., INFO, DEBUG
+    file: Path  # Path to log file
+
 
 class DBConfig(BaseModel):
-    schema_file: FilePath # SQL DDL file
-    db_path: Path # SQLite DB path
+    schema_file: FilePath  # SQL DDL file
+    db_path: Path  # SQLite DB path
+
 
 class Settings(BaseModel):
     database: DBConfig
